c5959824ece0de496a23cc9b96b53c70ecd82b0b
Ahhasha <elenacheoai@gmail.com>
2026-01-15 23:03:12 +0300

    feat: добавил start handler

---
+++ internal/delivery/telegram/handlers/start/handler.go
@@ -0,0 +1,22 @@
+package start
+
+import (
+	"context"
+
+	"github.com/Ahhasha/Tracker-bot/internal/model"
+)
+
+type Handler struct{}
+
+func New() *Handler {
+	return &Handler{}
+}
+
+func (h *Handler) Handle(ctx context.Context, cmd *model.Command) (model.Result, error) {
+	_ = ctx
+
+	return model.Result{
+		ChatID: cmd.ChatID,
+		Text:   "Саламчииик",
+	}, nil
+}
