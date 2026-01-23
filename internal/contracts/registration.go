df13a8aa51218a3086bc22860cb12f1511f9c69f
Ahhasha <elenacheoai@gmail.com>
2026-01-23 20:21:27 +0300

    refactor: упростил контракт RegistrationRepo

--- internal/contracts/registration.go
+++ internal/contracts/registration.go
@@ -3,7 +3,6 @@ package contracts
 import "context"
 
 type RegistrationRepo interface {
-	UserExists(ctx context.Context, tgID int64) (bool, error)
-	Create(ctx context.Context, tgID int64, username string) (int64, error)
+	UpsertUser(ctx context.Context, tgID int64, username string) (userID int64, created bool, err error)
 	CreateDefaultCategory(ctx context.Context, userID int64) error
 }
